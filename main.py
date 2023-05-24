62c4539eb9244e8dda8161ee558766facf4fed1b
JanaRasras <jana.rasras@gmail.com>
2023-05-24 19:46:42 -0400

    [add] single connection

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ def main():
         add_node_images(fig, i_layer, image_axes, p, filler_image)
     add_output_image(fig, image_axes, p, filler_image)
     add_error_image(fig, image_axes, p, filler_image)
-    for i_layer, layer in enumerate(image_axes):
-        print(f"layer {i_layer - 1}: {len(layer)} Axes")
+    add_layer_connections(ax_boss, image_axes)
+    save_nn_viz(fig, "27_single_connection")
 
 def construct_parameters():
 
@@ -357,6 +357,26 @@ def add_filler_image(ax, n_im_rows, n_im_cols, filler_image):
     right = left + n_im_cols
     fill_patch = filler_image[top: bottom, left: right]
     ax.imshow(fill_patch, cmap="inferno")
+
+def add_layer_connections(ax_boss, image_axes):
+    x_start = image_axes[0][0].get_position().x1
+    x_end = image_axes[1][0].get_position().x0
+
+    ax_start = image_axes[0][0]
+    ax_start_pos = ax_start.get_position()
+    y_start_min = ax_start_pos.y0
+    y_start_max = ax_start_pos.y1
+    y_start = (y_start_min + y_start_max) / 2
+
+    ax_end = image_axes[1][0]
+    ax_end_pos = ax_end.get_position()
+    y_end_min = ax_end_pos.y0
+    y_end_max = ax_end_pos.y1
+    y_end = (y_end_min + y_end_max) / 2
+
+    x = [x_start, x_end]
+    y = [y_start, y_end]
+    ax_boss.plot(x, y, color=TAN)
 
 
 if __name__ == "__main__":
